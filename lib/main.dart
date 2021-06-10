80be7c70c4d71df4e659537f0fe30ccab82a0265
Asher Hwang <dev@hsh.kr>
2021-06-10 09:57:17 +0900

    Add image import from assets

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+/// You can remove _Screen widget to reset project when you use this template.
+class _Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Center(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: Image.asset(
+              'images/moa.jpg',
+              width: 100,
+              height: 100,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: Center(
+            child: Text(
+              'Hello Flutter!',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hello flutter',
       theme: ThemeData(primaryColor: Colors.blue),
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Hello world!',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-        ),
-      ),
+      home: Scaffold(body: _Screen()),
     );
   }
 }
